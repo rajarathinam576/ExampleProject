200e2c0157c57ecf991fd2282124768163badbf1
rajarathinam576 <rajarathinam.ayyanar@gmail.com>
2023-09-18 12:06:57 +0530

    gunacode done

--- src/org/git/Project.java
+++ src/org/git/Project.java
@@ -12,5 +12,11 @@ public class Project {
 		lis.add(40);
 		System.out.println(lis);
 	
+		List<Integer>list =new ArrayList<Integer>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		System.out.println(list);
 }
 }
