51782c421a32390641127619d8d38b408e82615c
rajarathinam576 <rajarathinam.ayyanar@gmail.com>
2023-09-18 11:23:54 +0530

    project code create

---
+++ src/org/git/Project.java
@@ -0,0 +1,16 @@
+package org.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+ public static void main(String[] args) {
+	 List<Integer>lis=new ArrayList<Integer>();
+		
+		lis.add(10);
+		lis.add(23);
+		lis.add(40);
+		System.out.println(lis);
+	
+}
+}
